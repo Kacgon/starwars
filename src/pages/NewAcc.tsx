4ad69531334af4863a108af93ea3f8ce7b91d838
Kacgon <ka.gonciarz@gmail.com>
2022-11-30 16:18:27 +0100

    Filtering of user likes and favs works

--- src/pages/NewAcc.tsx
+++ src/pages/NewAcc.tsx
@@ -9,14 +9,57 @@ window.onload = function() {
     GetAllFavedPlanets()
 } 
 
-function PeopleFilter() {
-    alert('asd')
-    return <h1>People like: {localStorage.getItem("LikedPeople")}</h1>
-
-}
-
-function NewAcc () {
+export function NewAcc () {
     const [chosen, setChosen] = useState("")
+    const [wybrane, setWybrane] = useState("")
+
+    function FilterHandler(): any{
+        if(chosen == " People"){
+            return <div>
+            <h1>People likes: {localStorage.getItem("LikedPeople")}</h1>
+            <h2>People Faved: {localStorage.getItem("FavedPeople")}</h2></div>
+        }else if(chosen == " Planets"){
+            return <div>
+            <h1>Planets likes: {localStorage.getItem("LikedPlanets")}</h1>
+            <h2>Planets Faved: {localStorage.getItem("FavedPlanets")}</h2></div>
+        }else if(chosen == " Films"){
+            return <div>
+            <h1>Films likes: {localStorage.getItem("LikedFilms")}</h1>
+            <h2>Films Faved: {localStorage.getItem("FavedFilms")}</h2></div>
+        }else if(chosen == " Species"){
+            return <div>
+            <h1>Species likes: {localStorage.getItem("LikedSpecies")}</h1>
+            <h2>Species faved: {localStorage.getItem("FavedSpecies")}</h2></div>
+        }else if(chosen == " Starships"){
+            return <div>
+            <h1>Starships likes: {localStorage.getItem("LikedStarships")}</h1>
+            <h2>Starships faved: {localStorage.getItem("FavedStarships")}</h2></div>
+        }else if(chosen == " Vechicle"){
+            return <div>
+            <h1>Vechicle likes: {localStorage.getItem("LikedVechicle")}</h1>
+            <h2>Vechicle faved: {localStorage.getItem("FavedVechicle")}</h2></div>
+        }else if(chosen ==" All"){
+            return <div><div>
+            <h1>People likes: {localStorage.getItem("LikedPeople")}</h1>
+            <h1>People Faved: {localStorage.getItem("FavedPeople")}</h1></div>
+            <div>   
+            <h1>Planets likes: {localStorage.getItem("LikedPlanets")}</h1>
+            <h1>Planets Faved: {localStorage.getItem("FavedPlanets")}</h1></div>
+            <div>
+            <h1>Films likes: {localStorage.getItem("LikedFilms")}</h1>
+            <h1>Films Faved: {localStorage.getItem("FavedFilms")}</h1></div>
+            <div>
+            <h1>Species likes: {localStorage.getItem("LikedSpecies")}</h1>
+            <h1>Species faved: {localStorage.getItem("FavedSpecies")}</h1></div>
+            <div>
+            <h1>Starships likes: {localStorage.getItem("LikedStarships")}</h1>
+            <h1>Starships faved: {localStorage.getItem("FavedStarships")}</h1></div>
+            <div>
+            <h1>Vechicle likes: {localStorage.getItem("LikedVechicle")}</h1>
+            <h1>Vechicle faved: {localStorage.getItem("FavedVechicle")}</h1></div>
+            </div>
+        }
+    }
     
     return (
     <div className="SignInButton"> <button className="SignInBut" onClick={signIn}>Sign in using Google</button>
@@ -25,6 +68,8 @@ function NewAcc () {
         onChange={(e)=>{
             const selectedType = e.target.value
             setChosen(selectedType)
+            setWybrane(selectedType)
+            
         }}>
             <option value=" People"> People</option>
             <option value=" Planets"> Planets</option>
@@ -32,22 +77,14 @@ function NewAcc () {
             <option value=" Species"> Species</option>
             <option value=" Starships"> Starships</option>
             <option value=" Vechicle"> Vechicle</option>
+            <option value=" All"> Show all</option>
         </select>
         {chosen}
         </div>
-    <div className="LoggedInText">Logged in : {localStorage.getItem("name")}</div>
-    <div className="Text">Faved People : {localStorage.getItem("FavedPeople")}</div>
-    <div className="Text">Liked People : {localStorage.getItem("LikedPeople")} </div>
-    <div className="Text">Faved Planets : {localStorage.getItem("FavedPlanets")} </div>
-    <div className="Text">Liked Planets : {localStorage.getItem("LikedPlanets")}</div>
-    <div className="Text">Liked Films : {localStorage.getItem("LikedFilms")}</div>
-    <div className="Text">Faved Films : {localStorage.getItem("FavedFilms")}</div>
-    <div className="Text">Liked Species : {localStorage.getItem("LikedSpecies")}</div>
-    <div className="Text">Faved Species : {localStorage.getItem("FavedSpecies")}</div>
-    <div className="Text">Liked Starships : {localStorage.getItem("LikedStarships")}</div>
-    <div className="Text">Faved Starships : {localStorage.getItem("FavedStarships")}</div>
-    <div className="Text">Liked Vechicle : {localStorage.getItem("LikedVechicle")}</div>
-    <div className="Text">Faved Vechicle : {localStorage.getItem("FavedVechicle")}</div>
+        <div>
+            <FilterHandler></FilterHandler>
+
+        </div>
     </div>
     )
     
