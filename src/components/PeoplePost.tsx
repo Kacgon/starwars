b61c900226d89c7b7eeac6da41289413a33d3310
Kacgon <ka.gonciarz@gmail.com>
2022-12-27 11:22:46 +0100

    Final version of counter, every feature works

--- src/components/PeoplePost.tsx
+++ src/components/PeoplePost.tsx
@@ -4,9 +4,10 @@ import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 import {db} from './Firebase'
 import { ref, push, remove, update, onValue, set, getDatabase, get, child} from "firebase/database";
 import {user} from '../pages/HomePage'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {GetAllFavedPeople, GetAllLikesPeople } from "../components/Firebase"
 import { Typography } from "@mui/material";
+
 
 export type PeoplePostProp = {
     name: string
@@ -15,6 +16,7 @@ export type PeoplePostProp = {
     hair_color:string
     skin_color: string
 }
+
 
 const addLike = (name: string) => {
     push(ref(db, user+ "/likedPeople/people/ " +name ), {
@@ -33,127 +35,134 @@ const addFav = (name: string) => {
 
 
 export function PeoplePost({name, height, mass, hair_color,skin_color}:
-PeoplePostProp) {
-    window.onload = GetCounterFromDB
-    
-    const[fav, setFav] = useState(false);
-    const favToggle=()=> {
-    setFav(!fav);
-}
-    const[like, setlike] = useState(false);
-    const likeToggle=()=> {
-    setlike(!like);
-}
+    PeoplePostProp) {
+        var [counter, setCounter] = useState()
+        function GetCounterFromDB(){
+            const dbRef = ref(db);
+            get(child(dbRef,name +'/likes/'))
+            .then(async (DataSnapshot)=> {
+                var Counter: any=[];
+                DataSnapshot.forEach(childSnapshot => {
+                    Counter.push(childSnapshot.val())
+                })
+            setCounter(Counter.length)
+        })    
+        }
+ 
 
-
-const DelFaved = () => {
-    remove(ref(db,user+ "/favedPeople/people/ " +name ), 
-    )
-}
-
-
-const DelLiked = () => {
-    remove(ref(db,user+ "/likedPeople/people/ " +name ), 
-    )
-}
-
-var [counterValue, setCounterValue] = useState(null)
-function GetCounterFromDB(){
-    const dbRef = ref(db);
-    get(child(dbRef,"CounterPeople/people/ " +name ))
-    .then((DataSnapshot)=> {
-        var Counter: any=[];
-        DataSnapshot.forEach(childSnapshot => {
-            Counter.push(childSnapshot.val())
-        })
-       setCounterValue(Counter)
+    useEffect(()=>{
+        GetCounterFromDB()
     })
-}
-
-var [counter, setCounter] = useState(1)
-function CounterHandler(){
-    if (liked == false){
-        setCounter(counter+1)
-    }else {
-        setCounter(counter-1)
+        
+        const[fav, setFav] = useState(false);
+        const favToggle=()=> {
+        setFav(!fav);
     }
-    update(ref(db,"CounterPeople/people/ " +name ), {
-        counter
-    }) 
-}
-
-
-
-const[liked, setLiked] = useState(true);
-function LikeHandler() {
-    if (liked == true) {
-        addLike(name)
-        GetAllLikesPeople()
-        setLiked(!liked)
-        localStorage.setItem('LIKED_STATUS'+name, JSON.stringify(liked))
-        likeToggle()
-        CounterHandler()
-        GetCounterFromDB()
-    } else {
-        DelLiked()
-        GetAllLikesPeople() 
-        setLiked(!liked)
-        localStorage.setItem('LIKED_STATUS'+name, JSON.stringify(liked))
-        likeToggle()
-        CounterHandler()
-        GetCounterFromDB()
+        const[like, setlike] = useState(false);
+        const likeToggle=()=> {
+        setlike(!like);
     }
-}
-
-const getLikeStatus : any = localStorage.getItem('LIKED_STATUS'+name)
-const LikeStatus = JSON.parse(getLikeStatus) 
-
-const[faved, setFaved] = useState(true);
-function FavoriteHandler() {
-    if (faved == true) {
-        favToggle()
-        addFav(name)
-        GetAllFavedPeople()
-        setFaved(!faved)
-    } else {
-        DelFaved()
-        GetAllFavedPeople()
-        favToggle()
-        setFaved(!faved)
+    
+    const DelFaved = () => {
+        remove(ref(db,user+ "/favedPeople/people/ " +name ), 
+        )
     }
-}
-if(localStorage.getItem("name") == null){
-    return <Card>
-        <Card.Body className="AppContainer" >
-            <Card.Title >
-    <span className="Text">{name}</span>
-            </Card.Title>
-        <Card.Body className="Text">height: <span>{height}</span></Card.Body>
-        <Card.Body className="Text">mass: <span>{mass}</span></Card.Body>
-        <Card.Body className="Text">hair_color: <span >{hair_color}</span></Card.Body>
-        <Card.Body className="Text">skin_color: <span>{skin_color}</span></Card.Body>
-        <h1 className="MustBeloggedinAlert">You heave to log in to like or fav posts</h1>
-        </Card.Body>
-    </Card>
-}else
-    return <Card>
-        <Card.Body className="AppContainer" >
-            <Card.Title >
-    <span className="Text">{name}</span>
-            </Card.Title>
-        <Card.Body className="Text">height: <span>{height}</span></Card.Body>
-        <Card.Body className="Text">mass: <span>{mass}</span></Card.Body>
-        <Card.Body className="Text">hair_color: <span >{hair_color}</span></Card.Body>
-        <Card.Body className="Text">skin_color: <span>{skin_color}</span></Card.Body>
-        <div className="like">
-        <FavoriteIcon onClick={LikeHandler} className={(!LikeStatus ? 'toggle-unliked' :'') + (LikeStatus ? 'toggle-liked':'')} 
-        ></FavoriteIcon>
-        <Typography>like counter: {counterValue}</Typography>
-        </div>
-        <div className="Favorite">
-        <BookmarkAddIcon onClick={FavoriteHandler} className={'toggle-unfaved' + (fav ? 'toggle-faved':'')}
-        ></BookmarkAddIcon>
-        </div>
-        </Card.Body>
-    </Card>
-}
+    
+    
+    const DelLiked = () => {
+        remove(ref(db,user+ "/likedPeople/people/ " +name ), 
+        )
+    }
+
+    
+    
+
+    function CounterHandler(){
+        if (liked == true){
+            push(ref(db,name +'/likes/' + user), {
+                user
+            }) 
+        }else {
+            remove(ref(db,name +'/likes/' + user))
+        
+        }
+    }
+    
+    
+    
+    const[liked, setLiked] = useState(true);
+    function LikeHandler() {
+        if (liked == true) {
+            addLike(name)
+            GetAllLikesPeople()
+            setLiked(!liked)
+            localStorage.setItem('LIKED_STATUS'+name, JSON.stringify(liked))
+            likeToggle()
+            CounterHandler()
+          
+        } else {
+            DelLiked()
+            GetAllLikesPeople() 
+            setLiked(!liked)
+            localStorage.setItem('LIKED_STATUS'+name, JSON.stringify(liked))
+            likeToggle()
+            CounterHandler()
+           
+        }
+    }
+    
+    const getLikeStatus : any = localStorage.getItem('LIKED_STATUS'+name)
+    const LikeStatus = JSON.parse(getLikeStatus) 
+    
+    const[faved, setFaved] = useState(true);
+    function FavoriteHandler() {
+        if (faved == true) {
+            favToggle()
+            addFav(name)
+            GetAllFavedPeople()
+            setFaved(!faved)
+        } else {
+            DelFaved()
+            GetAllFavedPeople()
+            favToggle()
+            setFaved(!faved)
+        }
+    }
+    
+
+    if(localStorage.getItem("name") == null){
+        return <Card>
+            <Card.Body className="AppContainer" >
+                <Card.Title >
+        <span className="Text">{name}</span>
+                </Card.Title>
+            <Card.Body className="Text">height: <span>{height}</span></Card.Body>
+            <Card.Body className="Text">mass: <span>{mass}</span></Card.Body>
+            <Card.Body className="Text">hair_color: <span >{hair_color}</span></Card.Body>
+            <Card.Body className="Text">skin_color: <span>{skin_color}</span></Card.Body>
+            <h1 className="MustBeloggedinAlert">You heave to log in to like or fav posts</h1>
+            </Card.Body>
+        </Card>
+    }else
+    
+        return <Card>
+            <Card.Body className="AppContainer" >
+                <Card.Title >
+        <span className="Text">{name}</span>
+                </Card.Title>
+            <Card.Body className="Text">height: <span>{height}</span></Card.Body>
+            <Card.Body className="Text">mass: <span>{mass}</span></Card.Body>
+            <Card.Body className="Text">hair_color: <span >{hair_color}</span></Card.Body>
+            <Card.Body className="Text">skin_color: <span>{skin_color}</span></Card.Body>
+            <div className="like">
+            <FavoriteIcon onClick={LikeHandler} className={(!LikeStatus ? 'toggle-unliked' :'') + (LikeStatus ? 'toggle-liked':'')} 
+            ></FavoriteIcon>
+            <Typography id="root">like counter: {counter}</Typography>
+            </div>
+            <div className="Favorite">
+            <BookmarkAddIcon onClick={FavoriteHandler} className={'toggle-unfaved' + (fav ? 'toggle-faved':'')}
+            ></BookmarkAddIcon>
+            </div>
+            </Card.Body>
+        </Card>
+    }
